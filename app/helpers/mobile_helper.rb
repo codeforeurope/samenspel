b3b5032e4907d3b251e5f4c3513590730b4f7d13
Giovanni Maggini <giovanni.maggini@gmail.com>
2014-03-27 18:53:57 +0100

    fixes to mobile Contacts pages

--- app/helpers/mobile_helper.rb
+++ app/helpers/mobile_helper.rb
@@ -2,7 +2,13 @@
 module MobileHelper
 
   def controller_selected?(controllers)
-    'selected' if Array(controllers).include? controller.controller_name
+    #This is done because in _project_navigation.m.haml, the People and Contacts links would both have "people" controller.controller_name and "selected" would not work //GM
+    if controllers == "contacts" || controllers == "people"
+        'selected' if Array(controllers).include? controller.action_name
+    else
+        'selected' if Array(controllers).include? controller.controller_name
+    end
+
   end
 
 end
